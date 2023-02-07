73384d086a0dc83ee40e8b5a8e8faca0f6bf9ecb
juliazom <ozmiteljul@gmail.com>
2023-02-07 12:05:24 +0000

    Added and styled comments section under an article

--- src/components/single/CommentItem.jsx
+++ src/components/single/CommentItem.jsx
@@ -1,3 +1,19 @@
 export const CommentItem = ({comment}) => {
-    return <li>{comment.author}</li>
+    const date = new Date(Date.parse(comment.created_at)).toLocaleString('en-GB', { timeZone: 'UTC'})
+
+    return <li className="comment-item">
+                <div className="header">
+                    <h3>{comment.author}</h3>
+                    <span className="date">{date}</span>
+                </div>
+                <p className="text">{comment.body}</p>
+                <div className="upvote">
+                    <button>Upvote</button>
+                    <button>Downvote</button>
+                    {comment.votes > 0 
+                        ? <span className="green">{comment.votes}</span> 
+                        : <span className="red">{comment.votes}</span>
+                    }
+                </div>
+            </li>
 }
--- src/components/single/CommentSection.jsx
+++ src/components/single/CommentSection.jsx
@@ -1,10 +1,10 @@
-import { CommentItem } from './CommentItem'
+import { CommentItem } from './CommentItem';
 
 export const CommentSection = ({comments}) => {
     return (
         <section className="comment-article">
           <h2>Comments</h2>
-            <ul>
+            <ul className='comment-list'>
                 {comments.map(comment => {
                     return <CommentItem key={comment.comment_id} comment={comment}/>}
                 )}
