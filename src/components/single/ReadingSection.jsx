6d36c3afed5a204ffcbedfd42f08c801e76e464c
juliazom <ozmiteljul@gmail.com>
2023-02-08 10:53:25 +0000

    Updated vote button

--- src/components/single/ReadingSection.jsx
+++ src/components/single/ReadingSection.jsx
@@ -4,24 +4,28 @@ import { IoMdThumbsUp, IoMdThumbsDown } from "react-icons/io";
 export const ReadingSection = ({article, setUpdatedArticle}) => {
     const date = new Date(Date.parse(article.created_at)).toLocaleString('en-GB', { timeZone: 'UTC'})
 
-    const [activeBtn, setActiveBtn] = useState("none");
+    const [activeBtn, setActiveBtn] = useState("");
 
     const handleVote = (e) => {
         e.preventDefault();
-        if (activeBtn === "none") {
+        if (activeBtn === "") {
             article.votes += 1
             setActiveBtn("like");
-            setUpdatedArticle (values => ({...values, inc_votes: +1})) 
+            setUpdatedArticle (values => ({...values, inc_votes: +1}));
             return;
         } else if (activeBtn === 'like') {
-            article.votes -= 2
-            setActiveBtn("dislike");
-            setUpdatedArticle (values => ({...values, inc_votes: -2})) 
+            article.votes -= 1
+            setActiveBtn("none");
+            setUpdatedArticle (values => ({...values, inc_votes: -1})); 
             return;
+        } else if (activeBtn === "none") {
+            article.votes -= 1
+            setActiveBtn("dislike");
+            setUpdatedArticle (values => ({...values, inc_votes: -1})); 
         } else if (activeBtn === "dislike") {
             article.votes += 1
-            setActiveBtn("none");
-            setUpdatedArticle (values => ({...values, inc_votes: +1})) 
+            setActiveBtn("");
+            setUpdatedArticle (values => ({...values, inc_votes: +1}));
         }
     }
        
@@ -37,7 +41,7 @@ export const ReadingSection = ({article, setUpdatedArticle}) => {
             <div className="vote">
                 <button 
                     onClick={handleVote}> 
-                    {activeBtn === "none" ? <span className="neutral"> <IoMdThumbsUp/> {article.votes}  <IoMdThumbsDown/> </span> : ''} 
+                    {!activeBtn || activeBtn === "none" ? <span className="neutral"> <IoMdThumbsUp/> {article.votes}  <IoMdThumbsDown/> </span> : ''} 
                     {activeBtn === "like" ? <span className="like-active"> Upvoted <IoMdThumbsUp fill="#0e8542" /> {article.votes}  </span> : ''} 
                     {activeBtn === "dislike" ? <span className="dislike-active"> Downvoted <IoMdThumbsDown fill="#FF0063" /> {article.votes} </span> : ''} 
                 </button>                
