5e0c9ccbd3573e83bdd71e881aaf88af5fa55500
juliazom <ozmiteljul@gmail.com>
2023-02-09 16:00:56 +0000

    Added options to sort articles by properties and by order

--- src/components/ArticlesListPage.jsx
+++ src/components/ArticlesListPage.jsx
@@ -3,23 +3,26 @@ import { useSearchParams } from "react-router-dom";
 import { getArticlesList, fetchTopics } from '../utils/api';
 import { ArticlesList } from './ArticlesList';
 import { ArticleTopics } from './ArticleTopics';
+import { ArticlesSortBy } from './ArticlesSortBy';
 import { LoadingItem } from './LoadingItem';
 
 export const ArticlesListPage = () => {
     const [topics, setTopics] = useState([]);
     const [articlesList, setArticlesList] = useState([]);
-    let [selectedTopic, setSelectedTopic] = useSearchParams('');
+    let [searchParams, setSearchParams] = useSearchParams();
     const [isLoading, setLoading] = useState(true);
     
-    let searchByTopic = selectedTopic.get('topic');
+    let topic = searchParams.get('topic');
+    let sortBy = searchParams.get('sort_by');
+    let order = searchParams.get('order');
 
     useEffect(() => {
-        getArticlesList(searchByTopic)
+        getArticlesList(topic, sortBy, order)
         .then(response => {
             setArticlesList(response.data.articles)
             setLoading(false)
         })
-    }, [searchByTopic]);
+    }, [topic, sortBy, order]);
 
     useEffect(() => {
         fetchTopics()
@@ -31,7 +34,8 @@ export const ArticlesListPage = () => {
     if (isLoading) return <LoadingItem />
     return (
         <main className='articles-page'>
-           <ArticleTopics topics={topics} setSelectedTopic={setSelectedTopic} />
+           <ArticleTopics topics={topics} setSelectedTopic={setSearchParams} />
+           <ArticlesSortBy setSelectedSortBy={setSearchParams} />
            <ArticlesList articles={articlesList} />
         </main>
     )
